4dbce38a0857b842adfba3a59c825898deb0bb23
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-12 19:41:24 -0600

    subFeature: API GET route to fetch all user orders from db
    use /users/:userId/orders to retrieve all orders for that user.

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,36 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User, Order },
+} = require("../db");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+// all routes in this file prepended with /api/users
+// so this can be reached via [project]/api/users/:userId/orders
+router.get("/:userId/orders", async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      // find all orders in db where userId matches the :userId url param
+      where: { userId: req.params.userId },
+    });
+    res.send(orders);
+  } catch (error) {
+    console.log(
+      `error in router.get route /api/users/:userId/orders: ${error}`
+    );
+    next(error);
+  }
+});
