e503d5405c26fca083899149dd33d5b66034eb91
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-13 21:32:14 -0600

    refactored Routes.js to use react-redux hooks
    i was trying to squash a bug but this was not the issue. but now it's done so might as well pull it in.

--- client/Routes.js
+++ client/Routes.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
@@ -11,58 +11,38 @@ import CartView from './components/CartView';
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+const Routes = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
 
-  render() {
-    const { isLoggedIn } = this.props;
+  useEffect(() => {
+    dispatch(me());
+  }, [isLoggedIn]);
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/products" exact component={AllProducts} />
-            <Route path="/products/:productId" component={SingleProduct} />
-            <Route path="/cart" component={CartView} />
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/products" exact component={AllProducts} />
-            <Route path="/products/:productId" component={SingleProduct} />
-            <Route path="/" exact component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/cart" component={CartView} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
-}
-
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
-};
-
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Switch>
+          <Route path="/products" exact component={AllProducts} />
+          <Route path="/products/:productId" component={SingleProduct} />
+          <Route path="/cart" component={CartView} />
+          <Route path="/home" component={Home} />
+          <Redirect to="/home" />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route path="/products" exact component={AllProducts} />
+          <Route path="/products/:productId" component={SingleProduct} />
+          <Route path="/" exact component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/cart" component={CartView} />
+        </Switch>
+      )}
+    </div>
+  );
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
+export default withRouter(Routes);
