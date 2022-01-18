5db13917cccfc205d0bdc1e067fdd61ee3d3181b
alstonwhite <59372363+alstonwhite@users.noreply.github.com>
2022-01-17 19:03:52 -0500

    Bootstrapify EditProduct component

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -11,7 +11,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import AccordionItem from "react-bootstrap/esm/AccordionItem";
 
 const AdminProductPanel = () => {
   const dispatch = useDispatch();
@@ -41,104 +40,74 @@ const AdminProductPanel = () => {
       type: "",
       description: "",
     });
-    toggleAddProduct();
   };
 
   const { name, price, imageUrl, type, description } = formData;
 
   return (
-    <div>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Add Product</Accordion.Header>
-          <Accordion.Body>
-            <Form onSubmit={handleSubmit}>
-              <FormGroup as={Row} controlId="formGridName">
-                <FormLabel>Name</FormLabel>
-                <FormControl
-                  type="text"
-                  name="name"
-                  onChange={handleChange}
-                  value={name}
-                />
-              </FormGroup>
-              <FormGroup as={Row} controlId="formGridPrice">
-                <FormLabel>Price</FormLabel>
-                <FormControl
-                  type="text"
-                  name="price"
-                  onChange={handleChange}
-                  value={price}
-                />
-              </FormGroup>
-              <FormGroup as={Row} controlId="formGridImageUrl">
-                <FormLabel>Image URL</FormLabel>
-                <FormControl
-                  type="text"
-                  name="imageUrl"
-                  onChange={handleChange}
-                  value={imageUrl}
-                />
-              </FormGroup>
-              <Form.Group as={Row} controlId="formGridType">
-                <Form.Label>Type</Form.Label>
-                <Form.Select
-                  name="type"
-                  defaultValue="plant"
-                  onChange={handleChange}
-                  value={type}
-                >
-                  <option value="plant">Plant</option>
-                  <option value="pot">Pot</option>
-                  <option value="tool">Tool</option>
-                  <option value="apparel">Apparel</option>
-                </Form.Select>
-              </Form.Group>
-              <Form.Group as={Row} controlId="formGridDescription">
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  name="description"
-                  onChange={handleChange}
-                  value={description}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-      {/* <form onSubmit={handleSubmit}>
-        <button type="button" onClick={handleCancel}>
-        Cancel
-        </button>
-        <label htmlFor="name">Name</label>
-        <input name="name" onChange={handleChange} value={name} />
-        <label htmlFor="price">Price</label>
-        <input name="price" onChange={handleChange} value={price} />
-        <label htmlFor="imageUrl">Image Url</label>
-        <input name="imageUrl" onChange={handleChange} value={imageUrl} />
-        <label htmlFor="type">Type</label>
-        <select name="type" onChange={handleChange} value={type}>
-          <option value="plant">Plant</option>
-          <option value="pot">Pot</option>
-          <option value="tool">Tool</option>
-          <option value="apparel">Apparel</option>
-        </select>
-        <label htmlFor="description">Description</label>
-        <textarea
-          name="description"
-          onChange={handleChange}
-          value={description}
-          rows="4"
-          cols="50"
-        />
-        <button type="submit">Submit</button>
-      </form> */}
-    </div>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Add Product</Accordion.Header>
+        <Accordion.Body>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup as={Row} controlId="formGridName">
+              <FormLabel>Name</FormLabel>
+              <FormControl
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={name}
+              />
+            </FormGroup>
+            <FormGroup as={Row} controlId="formGridPrice">
+              <FormLabel>Price</FormLabel>
+              <FormControl
+                type="text"
+                name="price"
+                onChange={handleChange}
+                value={price}
+              />
+            </FormGroup>
+            <FormGroup as={Row} controlId="formGridImageUrl">
+              <FormLabel>Image URL</FormLabel>
+              <FormControl
+                type="text"
+                name="imageUrl"
+                onChange={handleChange}
+                value={imageUrl}
+              />
+            </FormGroup>
+            <Form.Group as={Row} controlId="formGridType">
+              <Form.Label>Type</Form.Label>
+              <Form.Select
+                name="type"
+                defaultValue="plant"
+                onChange={handleChange}
+                value={type}
+              >
+                <option value="plant">Plant</option>
+                <option value="pot">Pot</option>
+                <option value="tool">Tool</option>
+                <option value="apparel">Apparel</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group as={Row} controlId="formGridDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="description"
+                onChange={handleChange}
+                value={description}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 };
 
--- client/components/EditProduct.js
+++ client/components/EditProduct.js
@@ -4,6 +4,15 @@ import { useParams } from "react-router-dom";
 import history from "../history";
 import { getProduct } from "../store/singleProduct";
 import { editProduct, deleteProduct } from "../store/products";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormLabel from "react-bootstrap/FormLabel";
+import FormControl from "react-bootstrap/FormControl";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const EditProduct = () => {
   const dispatch = useDispatch();
@@ -51,39 +60,72 @@ const EditProduct = () => {
   const { name, price, imageUrl, type, description } = formData;
 
   return (
-    <div>
+    <Container fluid>
       <h2>Edit Product</h2>
-      <form onSubmit={handleSubmit}>
-        <button type="button" onClick={handleBack}>
-          Back
-        </button>
-        <label htmlFor="name">Name</label>
-        <input name="name" onChange={handleChange} value={name} />
-        <label htmlFor="price">Price</label>
-        <input name="price" onChange={handleChange} value={price} />
-        <label htmlFor="imageUrl">Image Url</label>
-        <input name="imageUrl" onChange={handleChange} value={imageUrl} />
-        <label htmlFor="type">Type</label>
-        <select name="type" onChange={handleChange} value={type}>
-          <option value="plant">Plant</option>
-          <option value="pot">Pot</option>
-          <option value="tool">Tool</option>
-          <option value="apparel">Apparel</option>
-        </select>
-        <label htmlFor="description">Description</label>
-        <textarea
-          name="description"
-          onChange={handleChange}
-          value={description}
-          rows="4"
-          cols="50"
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <button type="button" onClick={handleDelete}>
+      <Button variant="secondary" onClick={handleBack}>
+        Back
+      </Button>
+      <br></br>
+      <Button variant="danger" onClick={handleDelete}>
         Delete
-      </button>
-    </div>
+      </Button>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup as={Row} controlId="formGridName">
+          <FormLabel>Name</FormLabel>
+          <FormControl
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={name}
+          />
+        </FormGroup>
+        <FormGroup as={Row} controlId="formGridPrice">
+          <FormLabel>Price</FormLabel>
+          <FormControl
+            type="text"
+            name="price"
+            onChange={handleChange}
+            value={price}
+          />
+        </FormGroup>
+        <FormGroup as={Row} controlId="formGridImageUrl">
+          <FormLabel>Image URL</FormLabel>
+          <FormControl
+            type="text"
+            name="imageUrl"
+            onChange={handleChange}
+            value={imageUrl}
+          />
+        </FormGroup>
+        <Form.Group as={Row} controlId="formGridType">
+          <Form.Label>Type</Form.Label>
+          <Form.Select
+            name="type"
+            defaultValue="plant"
+            onChange={handleChange}
+            value={type}
+          >
+            <option value="plant">Plant</option>
+            <option value="pot">Pot</option>
+            <option value="tool">Tool</option>
+            <option value="apparel">Apparel</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group as={Row} controlId="formGridDescription">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="description"
+            onChange={handleChange}
+            value={description}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
