dee6dd564c488e06ff952d99b8588f6a1f6c8d3e
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-19 14:45:05 -0600

    add admin panel to home for admin users

--- client/components/Home.js
+++ client/components/Home.js
@@ -24,59 +24,102 @@ const Home = () => {
   useEffect(() => {
     dispatch(getOrders(id));
   }, []);
+
   const orders = useSelector((state) => state.currentOrders);
   const { cart } = useSelector((state) => state);
+  const isAdmin = useSelector((state) => state.auth.type === 'admin');
 
   return (
     <Container>
-      <Alert variant="success" style={{ maxWidth: '25rem' }}>
-        Successfully logged in as {username}
-      </Alert>
-      <Col lg={8}>
-        <Card variant="success" border="success">
-          <Card.Body>
-            <Card.Title>Welcome, {username}</Card.Title>
-            <Card.Text>
-              Thank you for being a House of Plants customer!
-            </Card.Text>
-            <Button
-              variant="primary"
-              onClick={() => {
-                history.push('/products');
-              }}
-            >
-              Start Shopping
-            </Button>
-          </Card.Body>
-          <ListGroup>
-            <ListGroup.Item>
+      {isAdmin ? (
+        <Container fluid>
+          <Alert variant="success" style={{ maxWidth: '25rem' }}>
+            Successfully logged in as {username}. You have admin privileges.
+          </Alert>
+          <Col lg={8}>
+            <Card bg="warning">
+              <Card.Body>
+                <Card.Title>Admin Control Panel</Card.Title>
+                <Card.Text>You're the captain here:</Card.Text>
+              </Card.Body>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Button
+                    variant="warning"
+                    onClick={() => {
+                      history.push('/admin');
+                    }}
+                  >
+                    Edit Inventory
+                  </Button>{' '}
+                  Add, remove, and edit items in the HoP inventory.
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    variant="warning"
+                    onClick={() => {
+                      history.push('/users');
+                    }}
+                  >
+                    View Users
+                  </Button>{' '}
+                  House of Plants currently has XX registered customers.
+                </ListGroup.Item>
+                <ListGroup.Item disabled>
+                  <Button variant="secondary">Order History</Button> There have
+                  been a total of YY completed orders.
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Container>
+      ) : (
+        <Col lg={8}>
+          <Card variant="success" border="success">
+            <Card.Body>
+              <Card.Title>Welcome, {username}</Card.Title>
+              <Card.Text>
+                Thank you for being a House of Plants customer!
+              </Card.Text>
               <Button
-                variant="secondary"
+                variant="primary"
                 onClick={() => {
-                  history.push('/cart');
+                  history.push('/products');
                 }}
               >
-                View Cart
-              </Button>{' '}
-              Your cart currently contains {cart.length} unique items
-            </ListGroup.Item>
-            <ListGroup.Item disabled>
-              <Button disabled variant="secondary">
-                Order History
-              </Button>{' '}
-              You have placed{' '}
-              {orders.filter((order) => order.status === 'complete').length}{' '}
-              orders with House of Plants.
-            </ListGroup.Item>
-            <ListGroup.Item disabled>
-              <Button variant="secondary" disabled>
-                Edit Account
-              </Button>{' '}
-              Update your Account email address
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Col>
+                Start Shopping
+              </Button>
+            </Card.Body>
+            <ListGroup>
+              <ListGroup.Item>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    history.push('/cart');
+                  }}
+                >
+                  View Cart
+                </Button>{' '}
+                Your cart currently contains {cart.length} unique items
+              </ListGroup.Item>
+              <ListGroup.Item disabled>
+                <Button disabled variant="outline-secondary">
+                  Order History
+                </Button>{' '}
+                You have placed{' '}
+                {orders.filter((order) => order.status === 'complete').length}{' '}
+                orders with House of Plants.
+              </ListGroup.Item>
+              <ListGroup.Item disabled>
+                <Button variant="outline-secondary" disabled>
+                  Edit Account
+                </Button>{' '}
+                Update your Account email address
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      )}
     </Container>
   );
 };
