8f21ecf6757b41de08d7a10ea072d33247a4a007
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-19 13:20:19 -0600

    Home page shows unique cart items, number of previous orders

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
@@ -8,11 +8,25 @@ import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Col from 'react-bootstrap/Col';
 import history from '../history';
+import { getCart, addToCart } from '../store/cart';
+import { getOrders } from '../store/orders';
+
 /**
  * COMPONENT
  */
 const Home = () => {
+  const dispatch = useDispatch();
   const { id, username } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getCart(id));
+  }, []);
+
+  useEffect(() => {
+    dispatch(getOrders(id));
+  }, []);
+  const orders = useSelector((state) => state.currentOrders);
+  const { cart } = useSelector((state) => state);
 
   return (
     <Container>
@@ -29,7 +43,7 @@ const Home = () => {
             <Button
               variant="primary"
               onClick={() => {
-                history.pushState('/products');
+                history.push('/products');
               }}
             >
               Start Shopping
@@ -40,13 +54,15 @@ const Home = () => {
               <Button variant="secondary" disabled>
                 View Cart
               </Button>{' '}
-              Your cart currently has () items
+              Your cart currently contains {cart.length} unique items
             </ListGroup.Item>
             <ListGroup.Item disabled>
               <Button variant="secondary" disabled>
                 Order History
               </Button>{' '}
-              You have placed () orders with House of Plants.
+              You have placed{' '}
+              {orders.filter((order) => order.status === 'complete').length}{' '}
+              orders with House of Plants.
             </ListGroup.Item>
             <ListGroup.Item disabled>
               <Button variant="secondary" disabled>
