40708f8fbdce7a4b2d2e1915f88aeca1e30caac4
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-19 13:05:38 -0600

    Add Home user dashboard

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,24 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
-
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Col from 'react-bootstrap/Col';
+import history from '../history';
 /**
  * COMPONENT
  */
 const Home = () => {
-  const { id, email } = useSelector((state) => state.auth);
+  const { id, username } = useSelector((state) => state.auth);
 
   return (
     <Container>
       <Alert variant="success" style={{ maxWidth: '25rem' }}>
-        Successfully logged in as {email}
+        Successfully logged in as {username}
       </Alert>
-      <Card>
-        <Card.Title>Welcome, {email}</Card.Title>
-        <Card.Text>hi</Card.Text>
-      </Card>
+      <Col lg={8}>
+        <Card variant="success" border="success">
+          <Card.Body>
+            <Card.Title>Welcome, {username}</Card.Title>
+            <Card.Text>
+              Thank you for being a House of Plants customer!
+            </Card.Text>
+            <Button
+              variant="primary"
+              onClick={() => {
+                history.pushState('/products');
+              }}
+            >
+              Start Shopping
+            </Button>
+          </Card.Body>
+          <ListGroup>
+            <ListGroup.Item disabled>
+              <Button variant="secondary" disabled>
+                View Cart
+              </Button>{' '}
+              Your cart currently has () items
+            </ListGroup.Item>
+            <ListGroup.Item disabled>
+              <Button variant="secondary" disabled>
+                Order History
+              </Button>{' '}
+              You have placed () orders with House of Plants.
+            </ListGroup.Item>
+            <ListGroup.Item disabled>
+              <Button variant="secondary" disabled>
+                Edit Account
+              </Button>{' '}
+              Update your Account email address
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Container>
   );
 };
